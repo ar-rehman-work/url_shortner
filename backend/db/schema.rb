153830cfb80bbe90302e1ddee363c08e54a35002
Abdul Rehman <ar.rehman.work@gmail.com>
2026-05-08 21:43:14 +0500

    Added User Model

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_06_094922) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_08_153921) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,5 +20,14 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_06_094922) do
     t.string "short_code"
     t.datetime "updated_at", null: false
     t.index ["short_code"], name: "index_short_urls_on_short_code", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email", null: false
+    t.string "name", limit: 100, null: false
+    t.string "password_digest", null: false
+    t.datetime "updated_at", null: false
+    t.index "lower((email)::text)", name: "index_users_on_LOWER_email", unique: true
   end
 end
